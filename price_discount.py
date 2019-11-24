e64f8c4839a4639e30b27b2e0945e04b51194462
pmakukha <makukha.pavel@gmail.com>
2019-11-25 00:00:39 +0300

    поправил как работают циклы

--- price_discount.py
+++ price_discount.py
@@ -1,6 +1,6 @@
 
 
-
+# какую проблему решаем: снизить на 10% стоимость телефона с самым большим остатком на складе
 stock_items = [
   {'name' : 'iphone 5', 'price':'100', 'stock':'5' },
   {'name' : 'iphone 6', 'price':'200', 'stock':'10' },
@@ -8,32 +8,21 @@ stock_items = [
   {'name' : 'iphone 8', 'price':'400', 'stock':'30' },
   {'name' : 'iphone x', 'price':'500', 'stock':'40' }
 ]
-
-# что сделать
-# 1. вытащить  сток в список (?)
-# 2. найти значение макс
-# 3. в словаре найти стоимость модели с максималным остатком
-# 4. снизить эту стоимость на 10%
-
-
-#
-a=0
 stocks_list=[]
+#нахожу  значение самого большого остатока
 for x in stock_items:
-  #print(stock_items[a]['stock'])
-  stocks_list.append( int(stock_items[a]['stock']) )
-  a=a+1
-
-#print (stocks_list)
-#print ( max(stocks_list))
+  stocks_list.append( int(x['stock']) )
 max_stock =  max(stocks_list)
 
-a=0
+
+print(max_stock)
+
+#нахожу телефон с таким значением остатка и меняю цену
 for x in stock_items:
-  if  int(stock_items[a]['stock']) == max_stock:
-    print( 'Было ' + str (stock_items[a]))
-    stock_items[a]['price'] = int(stock_items[a]['price']) * 0.9 
-    print( 'Стало ' +str (stock_items[a]))
-  a=a+1
+    if  int (x ['stock'] )== max_stock:
+        print( 'Было ' + str (x) )
+        x ['price'] = int(x['price']) * 0.9 
+        print( 'Стало ' +str (x) )
+
 
 
